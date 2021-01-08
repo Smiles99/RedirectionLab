9b75ef638ca36db89c26b6b14426c69ee519d0a7
Smiles99 <mdavies10@stuy.edu>
2021-01-08 00:48:34 -0500

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -57,7 +57,39 @@ public class PigLatin {
     }
 
     public static String pigLatinBest(String s){
-        String latinized = "";
-        return latinized;
+      int x = 0;
+      int y = 1;
+      String latinized = "";
+      if(s.isLetter(s.charAt(x))) {
+        String hold = "";
+        hold = "" + s.charAt(x);
+        hold = hold.toLowerCase();
+        s = hold + s.substring(1);
+        if(s.length() >= 2 && (s.charAt(x) == 'b' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                (s.charAt(x) == 'c' && (s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                (s.charAt(x) == 'd' && s.charAt(y) == 'r') ||
+                (s.charAt(x) == 'f' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                (s.charAt(x) == 'g' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                (s.charAt(x) == 'n' && s.charAt(y) == 'g') ||
+                (s.charAt(x) == 'p' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                (s.charAt(x) == 'q' && s.charAt(y) == 'u') ||
+                (s.charAt(x) == 's' && (s.charAt(y) == 'c' || s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'm' ||
+                        s.charAt(y) == 'n' || s.charAt(y) == 'p' || s.charAt(y) == 't' || s.charAt(y) == 'w'))) {
+            hold = "" + s.charAt(x) + s.charAt(y);
+            s = s.substring(2);
+            latinized = s + hold + "ay";
+        } else {
+            if (s.charAt(x) == 'a' || s.charAt(x) == 'e' || s.charAt(x) == 'i' || s.charAt(x) == 'o' || s.charAt(x) == 'u') {
+                latinized = s + "hay";
+            } else {
+                hold = "" + s.charAt(x);
+                s = s.substring(1);
+                latinized = s + hold + "ay";
+            }
+        }
+      } else {
+        latinized = s;
+      }
+      return latinized;
     }
 }
