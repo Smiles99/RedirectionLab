cf2929f08079abba95523409b5b2a77bcbdabfdb
Smiles99 <mdavies10@stuy.edu>
2021-01-08 09:04:03 -0500

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -57,47 +57,47 @@ public class PigLatin {
     }
 
     public static String pigLatinBest(String s){
-      int x = 0;
-      int y = 1;
-      String latinized = "";
-      if(s.isLetter(s.charAt(x))) {
-        String hold1 = "";
-        String hold2 = "";
-        hold1 = "" + s.charAt(x);
-        hold1 = hold1.toLowerCase();
-        s = hold + s.substring(1);
-        if(s.length() >= 2 && (s.charAt(x) == 'b' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
-                (s.charAt(x) == 'c' && (s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
-                (s.charAt(x) == 'd' && s.charAt(y) == 'r') ||
-                (s.charAt(x) == 'f' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
-                (s.charAt(x) == 'g' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
-                (s.charAt(x) == 'n' && s.charAt(y) == 'g') ||
-                (s.charAt(x) == 'p' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
-                (s.charAt(x) == 'q' && s.charAt(y) == 'u') ||
-                (s.charAt(x) == 's' && (s.charAt(y) == 'c' || s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'm' ||
-                                        s.charAt(y) == 'n' || s.charAt(y) == 'p' || s.charAt(y) == 't' || s.charAt(y) == 'w'))) {
-            hold1 = "" + s.charAt(x) + s.charAt(y);
-            if (!(s.isLetter(s.charAt(s.length() - 1))) && !(s.isDigit(s.charAt(s.length() - 1)))) {
-              hold2 = s.charAt(s.length() - 1);
-              s = s.substring(2, s.charAt(s.length() - 1));
-              latinized = s + hold1 + "ay" + hold2;
+        int x = 0;
+        int y = 1;
+        String latinized = "";
+        if(Character.isLetter(s.charAt(x))) {
+            String hold1 = "";
+            String hold2 = "";
+            hold1 = "" + s.charAt(x);
+            hold1 = hold1.toLowerCase();
+            s = hold1 + s.substring(1);
+            if(s.length() >= 2 && (s.charAt(x) == 'b' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                    (s.charAt(x) == 'c' && (s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                    (s.charAt(x) == 'd' && s.charAt(y) == 'r') ||
+                    (s.charAt(x) == 'f' && (s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                    (s.charAt(x) == 'g' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                    (s.charAt(x) == 'n' && s.charAt(y) == 'g') ||
+                    (s.charAt(x) == 'p' && (s.charAt(y) == 'h' || s.charAt(y) == 'l' || s.charAt(y) == 'r')) ||
+                    (s.charAt(x) == 'q' && s.charAt(y) == 'u') ||
+                    (s.charAt(x) == 's' && (s.charAt(y) == 'c' || s.charAt(y) == 'h' || s.charAt(y) == 'k' || s.charAt(y) == 'l' || s.charAt(y) == 'm' ||
+                            s.charAt(y) == 'n' || s.charAt(y) == 'p' || s.charAt(y) == 't' || s.charAt(y) == 'w'))) {
+                hold1 = "" + s.charAt(x) + s.charAt(y);
+                if (!(Character.isLetter(s.charAt(s.length() - 1))) && !(Character.isDigit(s.charAt(s.length() - 1)))) {
+                    hold2 = "" + s.charAt(s.length() - 1);
+                    s = s.substring(2, s.charAt(s.length() - 1));
+                    latinized = s + hold1 + "ay" + hold2;
+                }
+            } else {
+                if (s.charAt(x) == 'a' || s.charAt(x) == 'e' || s.charAt(x) == 'i' || s.charAt(x) == 'o' || s.charAt(x) == 'u') {
+                    if (!(Character.isLetter(s.charAt(s.length() - 1))) && !(Character.isDigit(s.charAt(s.length() - 1)))) {
+                        hold2 = "" + s.charAt(s.length() - 1);
+                        s = s.substring(0, s.charAt(s.length() - 1));
+                        latinized = s +  "hay" + hold2;
+                    }
+                } else {
+                    hold1 = "" + s.charAt(x);
+                    s = s.substring(1);
+                    latinized = s + hold1 + "ay";
+                }
             }
         } else {
-            if (s.charAt(x) == 'a' || s.charAt(x) == 'e' || s.charAt(x) == 'i' || s.charAt(x) == 'o' || s.charAt(x) == 'u') {
-              if (!(s.isLetter(s.charAt(s.length() - 1))) && !(s.isDigit(s.charAt(s.length() - 1)))) {
-                hold2 = s.charAt(s.length() - 1);
-                s = s.substring(0, s.charAt(s.length() - 1));
-                latinized = s +  "hay" + hold2;
-              }
-            } else {
-                hold1 = "" + s.charAt(x);
-                s = s.substring(1);
-                latinized = s + hold + "ay";
-            }
+            latinized = s;
         }
-      } else {
-        latinized = s;
-      }
-      return latinized;
+        return latinized;
     }
 }
