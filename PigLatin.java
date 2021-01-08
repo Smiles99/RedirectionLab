4cd3f975f28235df575eb647f0f1cbde5390ec03
Smiles99 <mdavies10@stuy.edu>
2021-01-08 09:12:21 -0500

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -100,4 +100,23 @@ public class PigLatin {
         }
         return latinized;
     }
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        while (n.hasNextLine()) {
+            String line = n.nextLine();
+            String output = "";
+            Scanner m = new Scanner(line);
+            while (m.hasNext()) {
+                String input = m.next();
+                int x = 0;
+                String hold = "";
+                hold = pigLatinBest(input);
+                output = output + hold + " ";
+            }
+            m.close();
+            System.out.println(output);
+        }
+        n.close();
+    }
 }
